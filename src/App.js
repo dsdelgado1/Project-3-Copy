e4a6ab2d8e9f88b8e33da262d2771dac8164c5f3
Danny Delgado <ddelgado@wbwood.com>
2023-11-06 18:48:32 -0500

    nothing

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ const App = () => {
 
   useEffect(() => {
     if (role === 'CRM.Manage') {
-      console.log('ROlesddsa');
+      console.log('ROlesdddsa');
       dispatch(getCustomers());
     }
       // dispatch(getWorkerCustomers(workerId));
