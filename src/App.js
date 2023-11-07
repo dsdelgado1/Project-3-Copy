603039d31e7b596a7e12373a6b5082be9b810290
Danny Delgado <ddelgado@wbwood.com>
2023-11-06 19:09:24 -0500

    redirectUri from localhost to https://wbwcrmcpy.azurewebsites.net

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ const App = () => {
 
   useEffect(() => {
     if (role === 'CRM.Manage') {
-      console.log('ROlesdddsaaa');
+      console.log('ROlesdddsaaaaaaa');
       dispatch(getCustomers());
     }
       // dispatch  (getWorkerCustomers(workerId));
